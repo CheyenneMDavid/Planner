3ca6b9adbb81123c109cffc6f585d6d09358a4af
CheyenneMDavid <ctenmanikdavid@gmail.com>
2023-02-20 01:28:15 +0000

    Add google sheets as csv

--- run copy.py
+++ run copy.py
@@ -12,16 +12,53 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("far_east_takeaway")
+SHEET = GSPREAD_CLIENT.open("far_east")
 
 
 """
 This is the opening screen of the terminal, which greets the user, reminding
 them that the "Happy Customer always returns", both English and also cantonese
-reminding the user to convey an authnticity
+reminding the user to always convey an authnticity
 """
-print("           ======================================")
-print("                    快樂的客戶將永遠回來")
-print("           Welcome to Far East Takeaway Terminal")
-print("                 Happy Customer always returns!")
-print("           ======================================")
+print("                ======================================\n")
+print("                        快樂的客戶將永遠回來\n")
+print("                Welcome to Far East Takeaway Terminal\n")
+print("                  Happy Customer always returns!\n")
+print("                ======================================\n\n")
+
+df = pd.read_csv("far_east.csv")
+
+
+def user_name_terminal():
+    """
+    Request input for a single-word username with no spaces or special
+    characters.
+    The request will loop until the input is valid.
+    Change the username's first letter to capital.
+    When a valid user name is entered, the user is addressed by that name.
+    """
+    print("Please enter a valid single-word username that only consists of" "letters.")
+
+    print("Spaces and special characters are not allowed.\n")
+
+    while True:
+        username = input("Enter your username: ")
+
+        if not username.isalpha() or " " in username:
+            print(
+                "Please input a username that only consists of letters and"
+                "has no spaces."
+            )
+        else:
+            print("Welcome, " + username.capitalize() + "!")
+            return True
+
+
+def check_new_customer():
+    """
+    Use input telephone number to see if customer is already listed
+    """
+
+
+# Area in which functions are called
+user_name_terminal()
