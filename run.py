45872895894dcccb40d56b0688bcb87a8fd165dd
CheyenneMDavid <ctenmanikdavid@gmail.com>
2023-02-25 21:51:30 +0000

    Add function to check if customer is new or current

--- run.py
+++ run.py
@@ -14,12 +14,12 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("far_east")
 
-
 """
 This is the opening screen of the terminal, which greets the user, reminding
 them that the "Happy Customer always returns", both English and also cantonese
-reminding the user to always convey an authnticity
+reminding the user to always convey an authenticity
 """
+
 print("                ======================================\n")
 print("                        快樂的客戶將永遠回來\n")
 print("                Welcome to Far East Takeaway Terminal\n")
@@ -27,72 +27,73 @@ print("                  Happy Customer always returns!\n")
 print("                ======================================\n\n")
 
 
-"""
-Read Google sheet as a CSV fil in the root of application
-"""
-
-
 def user_name_terminal():
     """
     Request input for a single-word username with no spaces or special
-    characters.
-    The request will loop until the input is valid.
+    characters. The request will loop until the input is valid.
     Change the username's first letter to capital.
     When a valid user name is entered, the user is addressed by that name.
     """
-    print("Enter a valid single-word username that only consists of" "letters.")
-
+    print("Enter a valid single-word username")
+    print("Username must only consists of letters.")
     print("Spaces and special characters are not allowed.\n")
-
     while True:
         username = input("Enter your username: ")
 
         if not username.isalpha() or " " in username:
             print(
-                "Please input a username that only consists of letters and"
+                "Please input a username that only consists of letters and "
                 "has no spaces."
             )
         else:
             print("Welcome, " + username.capitalize() + "!")
-            return True
+            return username
 
 
-def get_valid_number(num):
+def get_valid_number():
     """
     Validate user input as a UK phone number, using imported regex module.
     While loop requesting input until input is valid.
     The Regex string does have short falls of coverage, but appears to be the
-    most comprehensive when using Python as a stand alone program
+    most comprehensive.
     """
+
     pattern = re.compile(
         r"^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|"
         r"((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|"
         r"((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$"
     )
 
-    return pattern.match(num)
-    print(num)
+    while True:
+        num = input("Please enter a valid UK phone number: \n")
+        if pattern.match(num):
+            print(f"Thank you, {num} is a valid phone number")
+            return num
+        else:
+            print(f"{num} is an invalid phone number")
 
 
-while True:
-    num = input("Please enter a valid UK phone number: \n")
-    if get_valid_number(num):
-        print("Phone number is valid")
-        break
+def customer_check(num):
+    """
+    Specify that data-type is a string.
+    Remove the preceeding "0", in order for it to be matched against the Phone
+    numbers column in the CSV, which doesn't store preceeding Zeros, without
+    extra jiggery-pokery in the settings of google sheets.
+    """
+    df = pd.read_csv("far_east.csv", dtype={"Phone number": str})
+
+    if num.startswith("0"):
+        num = num[1:]
+
+    result = df[df["Phone number"].str.endswith(num)]
+    if not result.empty:
+        print("Current customer")
     else:
-        print("Invalid phone number")
-
-
-def customer_check():
-    """
-    Check dataframe for matching phone number
-    """
-    df = pd.read_csv("far_east.csv")
-
-    print(df[["Phone number"]])
+        print("New customer. \n")
+        print("Enter new customer details. \n")
 
 
 # Area in which functions are called
-user_name_terminal()
-get_valid_number(num)
-customer_check()
+username = user_name_terminal()
+phone_number = get_valid_number()
+customer_check(phone_number)
