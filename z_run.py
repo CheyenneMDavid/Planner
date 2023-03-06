eb293f97fb56999c4cf703f5147f99923803ec88
CheyenneMDavid <ctenmanikdavid@gmail.com>
2023-03-06 04:19:46 +0000

    Add function to get string from user

--- z_run.py
+++ z_run.py
@@ -17,11 +17,11 @@ orders = SHEET.worksheet("orders")
 
 data = orders.get_all_values()
 
-print(data)
+# print(data)
 
 df = pd.read_csv("cakes.csv")
 
-print(df)
+# print(df)
 
 """
 Opening screen of the terminal greets user with the "Cakes RUs"
@@ -32,3 +32,21 @@ print("                ===================================\n")
 print("                      Welcome to Cakes R Us \n")
 print("                Happy Cake Customer always return!!\n")
 print("                ===================================\n\n")
+
+
+def get_user_name():
+    """
+    Request input for a single-word username with no spaces or special
+    characters.
+    """
+    print("Enter a valid single-word username")
+    print("Username must only consists of letters.")
+    print("Spaces and special characters are not allowed.")
+    print("Example:  'Dave' \n")
+
+    username = input("Enter your username: ")
+
+    print("Welcome, " + username.capitalize() + "!")
+
+
+get_user_name()
